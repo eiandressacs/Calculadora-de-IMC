447f1c5b8c8bd96623ea68fc61aaaac6d30a1b79
Carvalho <123520478+eiandressacs@users.noreply.github.com>
2023-08-09 15:08:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+/*const nome = 'Andressa'
+const sobrenome = 'Carvalho'
+const idade = 22
+const peso = 53
+const alturaEmM = 1.57 //ALTURA EM METROS
+
+const imc = peso / (alturaEmM * alturaEmM)
+
+console.log(nome + ' ' + sobrenome + ' tem ' + idade +
+' anos, e pesa ' + peso + 'kg. Tem altura de ' + alturaEmM +
+' e seu IMC é de ' + imc)*/
+
+
+const nome = 'Andressa'
+const sobrenome = 'Carvalho'
+const idade = 22
+const peso = 53
+const alturaEmM = 1.57 //ALTURA EM METROS
+const anoAtual = 2023
+
+let imc 
+imc = peso / (alturaEmM * alturaEmM)
+console.log(nome + ' ' + sobrenome + ' tem ' + idade +
+' anos, e pesa ' + peso + 'kg. Tem altura de ' + alturaEmM +
+' e seu IMC é de ' + imc)
+
+let anoDeNascimento
+anoDeNascimento = anoAtual - 23  
+console.log('Andressa tem 22 anos e nasceu no ano de ' + anoDeNascimento)
+
+//template strings ex: ${aquiFicaAVariavel}
